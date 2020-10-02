d12e073b0183a14c8e3735d35c5aacbb2792cd08
ernesto13 <esamaniego53@gmail.com>
2020-10-02 21:20:46 +0000

    added button to card, working on decrementing reps

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,54 @@
 $(document).ready(function(){
-
+// material design site https://mdbootstrap.com/md-bootstrap-cdn/
     let reps = 9;
 
-    function decrementReps(){
-        if(reps <= 0){
+//     $('.card-body .reps').on('click', function(){
 
-        }
-        reps--;
-        clickRep.html('Reps: ' + reps);
 
-    }
+//     if(reps <= 0){
+//         reps = 10;
+//         }
+//         else {
+//              reps--;
+//         $(this).html('Reps:' + reps);
+//         }
+// });
 
-    let clickRep = $('.reps');
-    clickRep.on('click', function(){
 
-        decrementReps();
-    })
+// $('.card-body .reps').each(function() {
+//     var i = 10;
+//     $(this).on('click', function(e) {
+//       var $parent = $(this).closest('.reps');
+//       var _vote = parseInt($parent.find('#reps').val());
+//       console.log(_reps + i++);
+//     });
+//   });
+
+
+
+
+
+//   $(document).on('click', '.reps', function(e) {
+
+//     var $parent = $(this).closest('.card-body');
+//     var _vote = parseInt($parent.find('#_reps').val()) - 1;
+//     $parent.find('#_reps').val(_vote)
+//     $parent.html('Reps: ' + _vote)
+
+//     console.log(_vote);
+//   });
+
+  $('.reps').each(function() {
+    var i = 0;
+    $(this).on('click', function(e) {
+      var $parent = $(this).closest('.card-body');
+      var _vote = parseInt($parent.find('#_reps').val());
+      var btn = $("<button class='btn btn-primary'>my btn</button>");
+      $parent.append("left: " + _vote + i--);
+      console.log(_vote + i--);
+ });
+  })
+
 });
+
+
